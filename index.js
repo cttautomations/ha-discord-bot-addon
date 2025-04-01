da14d9d705ab71a88a4f252488c0aa303fc3e2f8
cttautomations <ctt.automations@gmail.com>
2025-03-31 20:30:32 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.once('ready', () => {
-  console.log(`✅ Logged in as ${client.user.tag}`);
-});
-
-client.login(process.env.DISCORD_TOKEN);
